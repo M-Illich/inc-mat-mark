16c87069dc6d1b0dedb81f688113f75434786d15
M-Illich <moritz.illich@uni-ulm.de>
2026-07-16 16:14:59 +0200

    adapt test cases

--- src/main/java/eval/Evaluation.java
+++ src/main/java/eval/Evaluation.java
@@ -1,170 +1,277 @@
 package eval;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 import data.Fact;
+import data.Graph;
+import data.Update;
+import transform.DRedTransformer;
+import transform.FactExtracter;
+import transform.RuleReader;
+import transform.Transformer;
+import transform.UpdatesCreator;
 
 public class Evaluation {
 
-	/**
-	 * number of repeated runs to compute average runtime
-	 */
-	final static int REPETITIONS = 1; // TODO 5;
-
-	/**
-	 * "dred" or "bf" tests are possible
-	 */
-	static String TEST_ALGO = "dred"; // "bf"; //
-
-	/**
-	 * "ways" or TODO tests are possible
-	 */
-	static String TEST_TYPE = "ways";
-
-	/**
-	 * for "ways": 0 (GPS track0), 1 (GPS track1), or 2 (GPS track2)
-	 */
-	static int TEST_CASE = 1;
+	final static int REPETITIONS = 1; // TODO
+	final static Set<String> ALGORITHMS = Set.of("dred", "bf");
+	final static Set<String> TEST_CASES = Set.of("random", "random-large", "batch", "overlap", "scale-update",
+			"scale-data", "real");
+	final static Set<String> KNOWLEDGE_BASES = Set.of("path", "sequence", "claros", "dbpedia", "family", "lubm",
+			"relations", "ways");
+	final static List<Long> RANDOM_SEEDS = List.of(3682876523426446494l, -7227433133872759647l, -1521870568993095474l,
+			414674000676533665l, -5535749218636893965l, 7217840207545846233l, -433576350867050697l,
+			7450081150186899609l, -2232030332154254329l, 3341732484960904427l, 8087371546052459341l,
+			1169850498570268648l, -2050702418703038236l, 5572221214320583751l, -8362907299058540472l,
+			-3902863331811833142l, 7847669574699938735l, 8075569538961924907l);
 
 	public static void main(String[] args) throws Exception {
 
-		if (args.length >= 3) {
-			TEST_ALGO = args[0];
-			TEST_TYPE = args[1];
-			TEST_CASE = Integer.parseInt(args[2]);
+		// TODO use args
+		String algorithm = "dred";
+		String testCase = "real";
+		String knowledgeBase = "path";
+		int testRun = 0;
+
+		if (testRun < 0 || testRun > 2) {
+			throw new Exception("Invalid test run selection.");
 		}
 
-		if (TEST_ALGO != "dred" && TEST_ALGO != "bf") {
-			throw new Exception("Invalid test algorithm. Only \"dred\" or \"bf\" are possible");
+		if (testCase == "real") {
+			knowledgeBase = "ways";
 		}
 
-		System.out.println("test type: " + TEST_TYPE + "  --  test case: " + TEST_CASE);
+		performEvaluation(algorithm, testCase, knowledgeBase, testRun);
 
-		if (TEST_TYPE.contentEquals("ways")) {
-			if (TEST_CASE == 0 || TEST_CASE == 1 || TEST_CASE == 2) {
-				String updateFolder = "src/main/resources/updates/ways/updates_track" + TEST_CASE;
-				List<String> filesWays;
-				if (TEST_ALGO == "dred") {	// TODO "dred/dred_ways_no_mark.pl", 
-					filesWays = List.of("dred/dred_ways_mark_new.pl");
-				} else {
-					filesWays = List.of("bf/bf_ways_no_mark.pl", "bf/bf_ways_mark.pl");
+	}
+
+	private static void performEvaluation(String algorithm, String testCase, String knowledgeBase, int testRun)
+			throws Exception {
+
+		if (!ALGORITHMS.contains(algorithm)) {
+			throw new Exception("Invalid algorithm.");
+		}
+		if (!TEST_CASES.contains(testCase)) {
+			throw new Exception("Invalid test case.");
+		}
+		if (!KNOWLEDGE_BASES.contains(knowledgeBase)) {
+			throw new Exception("Invalid knowledge base.");
+		}
+
+		// default setting for random test case
+		int dataPoolSize = 200;
+		int updateNumber = 50;
+		List<Integer> initialUpdateSizes = new LinkedList<>(List.of(100));
+		List<Integer> updateSizes = new LinkedList<>(List.of(10));
+		int updateOverlapDel = 0;
+		int updateOverlapAdd = 0;
+		boolean asBatch = false;
+		boolean realTest = false;
+
+		switch (testCase) {
+		case "random-large":
+			dataPoolSize = 10000;
+			testRun += 3;
+			break;
+		case "batch":
+			asBatch = true;
+			testRun += 6;
+			break;
+		case "overlap":
+			updateOverlapDel = 5;
+			updateOverlapAdd = 5;
+			testRun += 9;
+			break;
+		case "scale-update":
+			updateNumber = 30;
+			initialUpdateSizes = new LinkedList<>(List.of(100, 100, 100, 100, 100, 100, 100, 100, 100, 100));
+			updateSizes = new LinkedList<>(List.of(5, 10, 15, 20, 25, 30, 35, 40, 45, 50));
+			testRun += 12;
+			break;
+		case "scale-data":
+			dataPoolSize = 2000;
+			updateNumber = 10;
+			initialUpdateSizes = new LinkedList<>(List.of(100, 200, 300, 400, 500));
+			updateSizes = new LinkedList<>(List.of(10, 20, 30, 40, 50));
+			testRun += 15;
+			break;
+		case "real":
+			realTest = true;
+		}
+
+		if (realTest) {
+			List<Update> updates = new UpdateStreamRun("",
+					"src/main/resources/updates/ways/updates_track" + testRun).updateList;
+
+			// prepare directories to store statistics
+			String directory = "results/" + algorithm + "/" + testCase + "/" + knowledgeBase + "/" + "updates_track"
+					+ testRun;
+			String testRunName = "";
+
+			// perform evaluation
+			processAlgorithms(algorithm, knowledgeBase, directory, testRunName, updates);
+		} else {
+			try {
+
+				long randomSeed = RANDOM_SEEDS.get(testRun);
+
+				for (int i = 0; i < updateSizes.size(); i++) {
+					int initialUpdateSize = initialUpdateSizes.get(i);
+					int updateSize = updateSizes.get(i);
+
+					// load data
+					System.out.println("Load data pool.");
+					LinkedHashSet<Fact> dataPool = loadDataPool(knowledgeBase, dataPoolSize);
+					System.out.println("  Data pool size: " + dataPool.size());
+
+					// create updates
+					System.out.println("Create updates.");
+					UpdatesCreator uc = new UpdatesCreator(dataPool);
+					List<Update> updates = uc.createRandomUpdates(updateNumber, initialUpdateSize, updateSize,
+							updateOverlapDel, updateOverlapAdd, asBatch, randomSeed);
+
+					// prepare directories to store statistics
+					String directory = "results/" + algorithm + "/" + testCase + "/" + knowledgeBase + "/" + randomSeed;
+					String testRunName = initialUpdateSize + "-" + updateSize + "-" + dataPoolSize;
+
+					// perform evaluation
+					processAlgorithms(algorithm, knowledgeBase, directory, testRunName + "/", updates);
 				}
-				for (String file : filesWays) {
-					performEvaluation(file, updateFolder);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * TODO
+	 * 
+	 * @param algorithm
+	 * @param knowledgeBase
+	 * @param directory
+	 * @param testRunName
+	 * @param updates
+	 */
+	private static void processAlgorithms(String algorithm, String knowledgeBase, String directory, String testRunName,
+			List<Update> updates) {
+		for (String approach : List.of("no_mark", "mark")) {
+			// get CHR code with algorithm
+			String chrFile = "src/main/prolog/" + algorithm + "/" + algorithm + "_" + knowledgeBase + "_" + approach
+					+ ".pl";
+			// process update stream
+			System.out.println("Approach: " + approach);
+			// collect statistics
+			List<Statistics> stats = new LinkedList<>();
+			for (int run = 1; run <= REPETITIONS; run++) {
+				System.out.println("Run: " + run);
+				UpdateStreamRun usr = new UpdateStreamRun(chrFile, updates);
+				usr.execute(false, false, false);
+				stats.add(usr.statistics);
+			}
+			// store statistics in files
+			storeStatistics(directory + "/" + approach + "/" + testRunName, stats);
+		}
+	}
+
+	/**
+	 * TODO
+	 * 
+	 * @param directory
+	 * @param stats
+	 */
+	private static void storeStatistics(String directory, List<Statistics> stats) {
+		try {
+			Files.createDirectories(Paths.get(directory));
+			PrintWriter writer;
+			String line;
+
+			// update times
+			writer = new PrintWriter(directory + "update_times" + ".csv", "UTF-8");
+			for (Statistics stat : stats) {
+				line = stat.updateTimes.toString();
+				line = line.substring(1, line.length() - 1);
+				writer.println(line);
+			}
+			writer.close();
+
+			// number of applied rules
+			for (String type : stats.get(0).appliedRules.keySet()) {
+				writer = new PrintWriter(directory + "/applied_rules_" + type + ".csv", "UTF-8");
+				for (Statistics stat : stats) {
+					line = stat.appliedRules.get(type).toString();
+					line = line.substring(1, line.length() - 1);
+					writer.println(line);
 				}
-			} else {
-				throw new Exception("Invalid test case. Only 0, 1, or 2 are allowed for \"ways\" test");
+				writer.close();
 			}
 
-		}
-		// TODO add further test cases
-//		else if (TEST_TYPE.contentEquals("cross")) {
-//			
-//		}
+			// number of marked facts
+			if (!stats.get(0).markedFacts.isEmpty()) {
+				for (String type : stats.get(0).markedFacts.keySet()) {
+					writer = new PrintWriter(directory + "/marked_facts_" + type + ".csv", "UTF-8");
+					for (Statistics stat : stats) {
+						line = stat.markedFacts.get(type).toString();
+						line = line.substring(1, line.length() - 1);
+						writer.println(line);
+					}
+					writer.close();
+				}
+			}
 
-		// invalid test type
-		else {
-			throw new Exception("Invalid test type. Only \"ways\" or TODO are allowed.");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
 
 	/**
-	 * Use the materialization maintenance approach implemented in {@code file} to
-	 * process a stream of updates based on provided updates. A csv-file is created,
-	 * which shows the cpu runtime, the number of applied rules for the algorithm's
-	 * phases, as well as the number of marked facts.
+	 * TODO
 	 * 
-	 * @param file         {@code String} name of file containing SWI-Prolog code to
-	 *                     be executed
-	 * @param updateFolder {@code String} name of folder where each stream update is
-	 *                     stored as file
+	 * @param knowledgeBase
+	 * @param dataPoolSize
+	 * @return
 	 */
-	public static void performEvaluation(String file, String updateFolder) {
+	private static LinkedHashSet<Fact> loadDataPool(String knowledgeBase, int dataPoolSize) {
+		LinkedHashSet<Fact> dataPool;
 
-		String approach = file.substring(file.indexOf("/") + 1, file.length() - 3);
-		String updatesName = updateFolder.substring(updateFolder.lastIndexOf("/") + 1);
-		System.out.println(approach);
-
-		PrintWriter writer;
-
-		try {
-			Files.createDirectories(Paths.get("results"));
-			// store evaluation results as table in a file
-			writer = new PrintWriter("results/results-" + approach + "-" + updatesName + ".csv", "UTF-8");
-
-			// create update stream
-			UpdateStreamRun usr = new UpdateStreamRun(file, updateFolder);
-
-			float avgStreamCpuTime = 0;
-			List<Long> avgUpdateCpuTimes = new LinkedList<>();
-			for (int i = 0; i < REPETITIONS; i++) {
-				// process update stream
-				usr.execute(false, false, false);
-				avgStreamCpuTime += usr.statistics.cpuTime;
-
-				// collect processing time for each update
-				if (i == 0) {
-					avgUpdateCpuTimes = usr.statistics.updateTimes;
-				} else {
-					for (int j = 0; j < usr.statistics.updateTimes.size(); j++) {
-						avgUpdateCpuTimes.set(j, avgUpdateCpuTimes.get(j) + usr.statistics.updateTimes.get(j));
-					}
-				}
+		if (knowledgeBase == "lubm") {
+			// OWL
+			File fileOWL = new File("src/main/resources/lubm/university.owl");
+			dataPool = FactExtracter.getFactsFromOWL(fileOWL);
+		} else if (knowledgeBase == "path" || knowledgeBase == "sequence") {
+			// create set of edges as facts
+			int nodesNum = 110;
+			while (dataPoolSize > nodesNum * nodesNum) {
+				nodesNum += 10;
 			}
-
-			// initialize table
-			writer.print("updates");
-			for (int i = 1; i <= avgUpdateCpuTimes.size(); i++) {
-				writer.print("," + i);
-			}
-			writer.println();
-			writer.print("CPU time [ms]");
-
-			// compute average processing time for each update
-			for (int i = 0; i < avgUpdateCpuTimes.size(); i++) {
-				avgUpdateCpuTimes.set(i, avgUpdateCpuTimes.get(i) / REPETITIONS);
-				writer.print("," + avgUpdateCpuTimes.get(i));
-			}
-			writer.println();
-			writer.print("materialization size");
-
-			// get materialization sizes
-			int avgMatSize = 0;
-			int maxMatSize = 0;
-			for (Set<Fact> mat : usr.queryAnswers) {
-				avgMatSize += mat.size();
-				if (mat.size() > maxMatSize) {
-					maxMatSize = mat.size();
-				}
-				writer.print("," + mat.size());
-			}
-			writer.println();
-			System.out.print("materialization sizes: " + (avgMatSize / usr.queryAnswers.size()) + " (avg), "
-					+ maxMatSize + " (max) facts");
-
-			// compute average runtime of whole stream
-			avgStreamCpuTime = avgStreamCpuTime / REPETITIONS;
-			System.out.println("");
-			System.out.println("average cpu time: " + avgStreamCpuTime + " seconds");
-			System.out.println("");
-
-			// get measured values from statistics
-			usr.statistics.appliedRules
-					.forEach((k, v) -> writer.println("applied rules (" + k + ")," + v.substring(1, v.length() - 1).replaceAll(" ", "")));
-			usr.statistics.markedFacts
-					.forEach((k, v) -> writer.println("marked facts (" + k + ")," + v.substring(1, v.length() - 1).replaceAll(" ", "")));
-
-			writer.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
+			Graph graph = new Graph(nodesNum, dataPoolSize, 123456789);
+			dataPool = graph.edges;
+		} else {
+			// RDF
+			File fileRDF = new File("src/main/resources/" + knowledgeBase + "/" + knowledgeBase);
+			dataPool = FactExtracter.getFactsFromRDF(fileRDF, "TURTLE");
 		}
+
+		// ensure that only explicit facts are used for updates
+		RuleReader rr = new RuleReader(new File("src/main/resources/" + knowledgeBase + "/" + knowledgeBase + ".dlog"));
+		Transformer trf = new DRedTransformer(rr.rules);
+		dataPool.removeIf(f -> !trf.explicitPredicates.contains(f.predicate));
+		// limit size of data pool
+		int dataPoolOffset = 0;
+		dataPool = new LinkedHashSet<>(Arrays
+				.asList(Arrays.copyOfRange(dataPool.toArray(new Fact[dataPoolSize]), dataPoolOffset, dataPoolSize)));
+
+		return dataPool;
 	}
 
 }
